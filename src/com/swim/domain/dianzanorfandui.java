8e5c5391b4563445f38f7ff19c5fdc080eb2b57b
FlowerRui <cpuflower@163.com>
2017-12-02 17:18:21 +0800

    dqq

---
+++ src/com/swim/domain/dianzanorfandui.java
@@ -0,0 +1,28 @@
+package com.swim.domain;
+
+public class dianzanorfandui {
+	private int ccid;//帖子编号
+	private int uid;//用户编号
+	private int judge;//是否点赞过
+	
+	public int getCcid() {
+		return ccid;
+	}
+	public void setCcid(int ccid) {
+		this.ccid = ccid;
+	}
+	public int getUid() {
+		return uid;
+	}
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+	public int getJudge() {
+		return judge;
+	}
+	public void setJudge(int judge) {
+		this.judge = judge;
+	}
+	
+
+}
---
+++ src/com/swim/idao/idianzandao.java
@@ -0,0 +1,10 @@
+package com.swim.idao;
+
+import java.sql.SQLException;
+
+import com.swim.domain.User;
+import com.swim.domain.dianzanorfandui;
+
+public interface idianzandao {
+	public dianzanorfandui DianZan(User user)throws ClassNotFoundException, SQLException;
+}
